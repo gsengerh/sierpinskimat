c21d9a09990848ad84679e86235d0f9a2019680e
Herbert Gsenger <herbert.gsenger@d-labs.biz>
2021-01-25 21:27:21 +0100

    Add function description and error handling.

--- makeSierpinskiCarpet.m
+++ makeSierpinskiCarpet.m
@@ -1,4 +1,18 @@
 function res = makeSierpinskiCarpet(iter)
+% MAKESIERPINSKICARPET Create a 3^iter x 3^iter Sierpinski carpet
+% Inputs:
+%   iter: number of iterations; must be >= 0
+% Outputs:
+%   res:  logical square matrix of size 3 ^ iter
+
+  if ~isnumeric(iter) || (iter ~= floor(iter)) || ~isscalar(iter)
+    error('Error. Input must be a scalar integer.');
+  end
+  
+  if iter < 0
+    error('Error. Input must be non-negative.');
+  end
+  
   if iter == 0
     res = 1;
     return
@@ -14,3 +28,4 @@ function res = makeSierpinskiCarpet(iter)
     right = bottom = mid + offset;
     res(left : right, top : bottom) = false;
   end
+end
